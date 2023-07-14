43463e0aa4f528dba1724bc437660ede72422cbb
Walking on the moon <45702832+Dredg@users.noreply.github.com>
2023-07-14 18:03:05 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,14 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+
+for (var i = 0; i < pronoun.length; i++) {
+  for (var j = 0; j < adj.length; j++) {
+    for (var k = 0; k < noun.length; k++) {
+      console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+    }
+  }
+}
